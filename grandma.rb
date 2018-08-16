30122013684296ef76cedeb17a528d107a81af0c
Derrick Reyes <derrickreyes@me.com>
2018-08-16 21:55:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!".upcase
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     puts "NO, NOT SINCE 1938!"
